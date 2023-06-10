91d8a91ce198c6884fc339ad197273b390fde1e7
Javed Ansari <javedans2003@gmail.com>
2023-06-11 00:23:58 +0530

    finally getting somewhere

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
 const myButton = document.querySelector(".btn-generate");
-myButton.Onclick = () => {
+myButton.addEventListener('click', () => {
   createDivs();
-};
+  console.log("hello world")
+})  
 
 function createDivs() {
   var numDivs = parseInt(document.getElementById("Lift-Input").value);
 
   var container = document.getElementById("lifts");
-  container.innerHTML = "";
-
+  console.log(container)
   for (var i = 0; i < numDivs; i++) {
     var div = document.createElement("div");
     div.className = "lift";
