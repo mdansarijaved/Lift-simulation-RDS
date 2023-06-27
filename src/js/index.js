cb93f9af9572ae3266cb50a9cd6ee16624d8340f
Javed Ansari <javedans2003@gmail.com>
2023-06-27 19:39:08 +0530

    finally did it

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,19 @@
-// style="transform: translateY(-126px); transition: transform 1500ms ease-in-out 0s;"
-
-
 let Floor = document.getElementById("Floor-input");
 let Lift = document.getElementById("Lift-Input");
 let submit = document.getElementById("btn-generate");
 let floorsContainer = document.querySelector("#adding-floors");
-let lift = document.createElement("div");
+let liftPositions = [];
 
 submit.addEventListener("click", () => {
-  const inputFloor = Floor.value;
-  const inputLift = Lift.value;
+  const inputFloor = +Floor.value;
+  const inputLift = +Lift.value;
   if (inputFloor < inputLift) {
     alert("floor cannot be less than lift");
     return;
   }
-  if (inputFloor < 0 || inputLift < 0){
-    alert("Enteries cannot be negative"); 
-    return ; 
+  if (inputFloor < 0 || inputLift < 0) {
+    alert("Entries cannot be negative");
+    return;
   }
   const screenWidth = window.innerWidth;
   if (screenWidth < 600 && inputLift > 3) {
@@ -27,27 +24,34 @@ submit.addEventListener("click", () => {
   while (floorsContainer.firstChild) {
     floorsContainer.firstChild.remove();
   }
-  for (let i = 0; i < inputFloor; i++) {
+  liftPositions = [];
+
+  for (let i = inputFloor; i > 0 ; i--) {
     let mainDiv = document.createElement("div");
     let buttonDiv = document.createElement("div");
     let buttonUp = document.createElement("button");
     let buttonDown = document.createElement("button");
     let liftDiv = document.createElement("div");
     liftDiv.classList.add("lifts");
-   
+
     mainDiv.classList.add("floor");
+    mainDiv.id = "floor" + i;
     buttonDiv.classList.add("btn-div");
-    buttonUp.classList.add("buttons");
+    buttonUp.id = i; 
+    buttonDown.id = i; 
+    buttonUp.classList.add("buttons", "buttonup");
     buttonUp.textContent = "UP";
     buttonDown.textContent = "DN";
-    buttonDown.classList.add("buttons");
-    if (i === 0) {
+    buttonDown.classList.add("buttons",  "buttondown");
+
+    if (i === inputFloor) {
       buttonDiv.appendChild(buttonDown);
-    } else if (i === inputFloor - 1) {
+    } else if (i === 1) {
       for (let j = 0; j < inputLift; j++) {
         let lift = document.createElement("div");
-        lift.classList.add("lift");
+        lift.classList.add("lift", "lift" + 1);
         liftDiv.appendChild(lift);
+        liftPositions.push(0);
       }
       buttonDiv.appendChild(buttonUp);
     } else {
@@ -58,7 +62,42 @@ submit.addEventListener("click", () => {
     mainDiv.appendChild(liftDiv);
     floorsContainer.appendChild(mainDiv);
 
+    // buttonsall.forEach((buttonsall) => {
+    // buttonsall.addEventListener("click", (e) => {
+    //   console.log(buttonsall)
+    //   const lifts = Array.from(document.querySelectorAll('.lift'));
+    //   const translationDistance = (inputFloor - i -1) * 120;
+    //   const currentPosition = liftPositions[activeLiftIndex];
+    //   const newPosition = currentPosition + translationDistance;
+    //   lifts[activeLiftIndex].style.transform = `translateY(-${newPosition}px)`;
+    //   lifts[activeLiftIndex].style.transition = "transform 1500ms ease-in-out 0s";
+    //   liftPositions[activeLiftIndex] = newPosition;
+    //   activeLiftIndex = (activeLiftIndex + 1) % lifts.length;
+    //   console.log(liftPositions)
+    // });
+    // });
   }
+  let activeLiftIndex = 0;
+  let buttonsall = Array.from(document.querySelectorAll(".buttons"));
+  buttonsall.forEach((buttonsall) => {
+    buttonsall.addEventListener("click", (e) => {
+      const floorvalue = parseInt(buttonsall.id); 
+      console.log(floorvalue)
+      const lifts = Array.from(document.querySelectorAll('.lift'));
+      const translatioindistance = (floorvalue-1) * 120; 
+      lifts[activeLiftIndex].style.transform = `translateY(-${translatioindistance}px)`;
+      lifts[activeLiftIndex].style.transition = "transform 1500ms ease-in-out 0s";
+      activeLiftIndex = (activeLiftIndex + 1) % lifts.length; 
+    });
+  });
 
 
 });
+
+const regenerate = document.querySelector('#btn-regenerate')
+
+regenerate.addEventListener('click',()=>{
+  while (floorsContainer.firstChild) {
+    floorsContainer.firstChild.remove();
+  }
+})
