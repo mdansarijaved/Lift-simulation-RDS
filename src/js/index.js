d0d45a909480003f970a47bd62b0a7cec03272d9
Javed Ansari <javedans2003@gmail.com>
2023-07-05 23:11:01 +0530

    update 2

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ let liftsState = [];
 submit.addEventListener("click", () => {
   const inputFloor = +Floor.value;
   const inputLift = +Lift.value;
-  // Checking(inputFloor, inputLift);
+ 
   if (inputFloor < inputLift) {
     alert("floor cannot be less than lift");
     return;
@@ -75,26 +75,7 @@ regenerate.addEventListener("click", () => {
 });
 
 
-// const Checking = (inputFloor,inputLift) => {
-//   console.log(inputFloor,inputLift)
-//   if (inputFloor < inputLift) {
-//     alert("floor cannot be less than lift");
-//     return;
-//   }
-//   if (inputFloor < 0 || inputLift < 0) {
-//     alert("Entries cannot be negative");
-//     return;
-//   }
-  
-//   const screenWidth = window.innerWidth;
-//   if (screenWidth < 600 && inputLift > 3) {
-//     alert("On mobile, input lift cannot be more than 3");
-//     return;
-//   }
-//   while (floorsContainer.firstChild) {
-//     floorsContainer.firstChild.remove();
-//   }
-// }
+
 
 const Createfloor = (inputFloor,inputLift) =>{
   for (let i = inputFloor; i > 0; i--) {
